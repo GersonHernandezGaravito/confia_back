7d92dfa554c944a1ba9c0b3f7ef9bb1d204ff9e9
GersonHernandezGaravito <ghernandezg13@miumg.edu.gt>
2021-05-21 15:16:26 -0600

    configuración BBDD

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://admin:admin@confiadb.6pt87.mongodb.net/myFirstDatabase?authSource=admin&replicaSet=atlas-bhiv4z-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true', {
     useNewUrlParser : true,
+    
     useUnifiedTopology : true
 })
 
